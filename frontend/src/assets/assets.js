c3405e4729a7e947e7cb3955a8c297d1bef2013d
Rishabh <rishabhm.10.03@gmail.com>
2025-03-06 21:40:07 +0530

    Added the hero section

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,6 +6,7 @@ import profile_icon from './profile_icon.png'
 import search_icon from './search_icon.png'
 import menu_icon from './menu_icon.png'
 import cross_icon from './cross_icon.png'
+import hero_img from './hero_image.jpg'
 
 export const assets = {
     logo,
@@ -25,5 +26,6 @@ export const assets = {
     // contact_img,
     // razorpay_logo,
     // stripe_logo,
-    cross_icon
+    cross_icon,
+    hero_img
 }
