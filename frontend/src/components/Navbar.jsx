c5a1e6ec93541dbe043c1f6d8a2703cac01dbea5
Rishabh <rishabhm.10.03@gmail.com>
2025-03-06 18:15:57 +0530

    Added responsiveness and a dropdown menu in the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
+    const [visible, setVisible] = useState(false)
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <img src={assets.logo} className='w-36' />
@@ -45,6 +46,19 @@ function Navbar() {
                         className='w-5 cursor-pointer'
                         src={assets.profile_icon}
                     />
+                    <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                        <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                            <p className='cursor-pointer hover:text-black'>
+                                My Profile
+                            </p>
+                            <p className='cursor-pointer hover:text-black'>
+                                Orders
+                            </p>
+                            <p className='cursor-pointer hover:text-black'>
+                                Logout
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <Link to='/cart' className='relative'>
                     <img src={assets.cart_icon} className='w-5 min-w-5' />
@@ -54,6 +68,54 @@ function Navbar() {
                     src={assets.menu_icon}
                     className='w-5 cursor-pointer sm:hidden'
                 />
+            </div>
+
+            {/* Sidebar menu for small screens */}
+            <div
+                className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+                    visible ? 'w-full' : 'w-0'
+                }`}
+            >
+                <div className='flex flex-col text-gray-600'>
+                    <div
+                        onClick={() => setVisible(false)}
+                        className='flex items-center gap-4 p-3 cursor-pointer'
+                    >
+                        <img
+                            className='h-4 rotate-180'
+                            src={assets.dropdown_icon}
+                        />
+                        <p>Back</p>
+                    </div>
+                    <NavLink
+                        onClick={() => setVisible(false)}
+                        className='py-2 pl-6 border'
+                        to='/'
+                    >
+                        HOME
+                    </NavLink>
+                    <NavLink
+                        onClick={() => setVisible(false)}
+                        className='py-2 pl-6 border'
+                        to='/collection'
+                    >
+                        COLLECTION
+                    </NavLink>
+                    <NavLink
+                        onClick={() => setVisible(false)}
+                        className='py-2 pl-6 border'
+                        to='/about'
+                    >
+                        ABOUT
+                    </NavLink>
+                    <NavLink
+                        onClick={() => setVisible(false)}
+                        className='py-2 pl-6 border'
+                        to='/contact'
+                    >
+                        CONTACT
+                    </NavLink>
+                </div>
             </div>
         </div>
     )
